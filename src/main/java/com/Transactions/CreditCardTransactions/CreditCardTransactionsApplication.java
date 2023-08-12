914cbd6a91de669246288daf4fd47291db8ca06c
jihenekhouja <61019520+jihenekhouja@users.noreply.github.com>
2023-08-12 06:37:47 +0100

    premiere commite

---
+++ src/main/java/com/Transactions/CreditCardTransactions/CreditCardTransactionsApplication.java
@@ -0,0 +1,12 @@
+package com.Transactions.CreditCardTransactions;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CreditCardTransactionsApplication {	
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardTransactionsApplication.class, args);
+	}
+	
+}
